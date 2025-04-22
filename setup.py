63c532cf966e4f0eedd28215e57b942dd1e7d772
月華 <81902542+getuka@users.noreply.github.com>
2025-04-22 21:02:39 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,16 @@ setup(
         'canary_tts', 'canary_tts.*'
     ]),
     install_requires=[
-        "torch",
-        "torchvision",
-        "torchaudio",
+        'torch',
+        'torchvision',
+        'torchaudio',
         'torchao',
         'torchtune',
         'transformers==4.44.2',
         'einops',
         'vector-quantize-pytorch',
+        'protobuf'
+        'accelerate'
     ],
     dependency_links=[
         'git+https://github.com/getuka/RubyInserter.git#egg=RubyInserter',
